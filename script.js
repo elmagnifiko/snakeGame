972c4f5be2e02dbe538eda121a33fcdbc541ed33
elmagnifiko <abdoulrazakadamoubello@gmail.com>
2025-04-17 20:25:35 +0200

    add collision logic

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ window.onload = function () {
   var delay = 100;
   var snakee;
   var applee;
+  var widthInBlocks = canvasWidth/blockSize;
+  var heightInBlocks = canvasHeight/blockSize;
 
   init();
 
@@ -31,11 +33,16 @@ window.onload = function () {
   }
 
   function refreshCanvas() {
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     snakee.advance();
-    snakee.draw();
-    applee.draw;
-    setTimeout(refreshCanvas, delay);
+    if (snakee.checkCollision()) {
+      /*  Game Over */
+    }else {
+      ctx.clearRect(0,0, canvasWidth , canvasHeight);
+      snakee.draw();
+      applee.draw();
+      setTimeout(refreshCanvas, delay);
+    }
+
   }
 
   function drawBlock(ctx, position) {
@@ -96,6 +103,31 @@ window.onload = function () {
         this.direction = newDirection;
       }
     };
+    this.checkCollision = function () {
+      var wallCollision = false;
+      var snakeCollision = false;
+      var head = this.body[0];
+      var rest = this.body.slice(1);
+      var snakeX = head[0];
+      var snakeY = head[1];
+      var minX = 0;
+      var minY = 0;
+      var maxX = widthInBlocks - 1;
+      var maxY = heightInBlocks - 1;
+      var isNotBetweenHorizontalWalls = snakeX < minX || snakeX > maxX;
+      var isNotBetweenVerticalWalls = snakeY < minY || snakeY > maxY;
+
+      if (isNotBetweenHorizontalWalls || isNotBetweenVerticalWalls) {
+        wallCollision = true;
+      }
+
+      for (var i = 0; i < rest.length; i++){
+        if (snakeX === rest[i][0] && snakeY === rest[i][1]) {
+          snakeCollision = true;
+        }
+      }
+      return wallCollision || snakeCollision;
+    }
   }
 
   function Apple(position) {
